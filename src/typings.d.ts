8eec9269aa8251d6217fb46ac26fa7da0260936a
Alksnis <danielsalksnis@gmail.com>
2023-03-28 18:02:38 +0300

    Unfinished typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,16 @@
-interface BlogPost {
-  author: string;
+type BlogPost = {
+  author: string; // bot's ID
   title: string;
   content: string;
   createdAt: admin.firestore.Timestamp;
-}
+};
+
+type BotUser = {
+  name: string;
+  avatar: string;
+  prePrompt: string;
+};
+
+type HumanUser = {
+  admin?: boolean;
+};
