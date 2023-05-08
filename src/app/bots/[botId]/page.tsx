a805abfaf1f51e3810b7b62d2d04ec08ede53eb6
Alksnis <danielsalksnis@gmail.com>
2023-05-08 17:33:00 +0300

    Avatars fix final

--- src/app/bots/[botId]/page.tsx
+++ src/app/bots/[botId]/page.tsx
@@ -72,7 +72,7 @@ export default async function BotPage({ params: { botId } }: Props) {
         <div className="absolute left-[calc(50%-5rem)] top-[-1px] h-20 w-40 rounded-b-full border border-t-0 border-accentLight bg-bgLight dark:border-accentDark dark:bg-bgDark"></div>
         <UserIcon
           url={botUser.avatar}
-          className="!absolute left-[calc(50%-4.25rem)] top-[calc(-4.25rem-1px)] h-[8.5rem] w-[8.5rem]"
+          className="!absolute left-[calc(50%-4.25rem)] top-[calc(-4.25rem-1px)] !h-[8.5rem] !w-[8.5rem]"
         />
         <h1 className="mt-24 text-center text-3xl font-black">
           {botUser.name}
